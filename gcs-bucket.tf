1f753c7a7be5df4c5048f6e3140e042de3ff36f0
Mayur <99334268+devopsmayur@users.noreply.github.com>
2023-04-08 23:04:47 +0530

    Update gcs-bucket.tf

--- gcs-bucket.tf
+++ gcs-bucket.tf
@@ -3,6 +3,6 @@ module "cloud-storage" {
   version = "3.4.1"
   # insert the 3 required variables here
   names = ["first", "second"]
-  prefix = "AI"
+  prefix = "mayur-unique-prefix"
   project_id = "hc-53771aab9cd94f93ba281d03ab6"
 }
