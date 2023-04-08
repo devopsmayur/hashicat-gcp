7fa1532ad8bf0d611c0ab85f66c50cf60eb3d7f3
Mayur <99334268+devopsmayur@users.noreply.github.com>
2023-04-08 23:02:30 +0530

    Update gcs-bucket.tf

--- gcs-bucket.tf
+++ gcs-bucket.tf
@@ -2,7 +2,7 @@ module "cloud-storage" {
   source  = "terraform-google-modules/cloud-storage/google"
   version = "3.4.1"
   # insert the 3 required variables here
-  names = "mayur1"
+  names = ["first", "second"]
   prefix = "AI"
   project_id = "hc-53771aab9cd94f93ba281d03ab6"
 }
