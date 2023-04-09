05feb0c246cfb0f297880027401f8df614ddc5b3
Mayur <99334268+devopsmayur@users.noreply.github.com>
2023-04-09 07:35:12 +0530

    Add files via upload

---
+++ gcs-bucket.tf
@@ -0,0 +1,8 @@
+module "cloud-storage" {
+  source  = "terraform-google-modules/cloud-storage/google"
+  version = "3.4.1"
+  # insert the 3 required variables here
+  names = ["first", "second"]
+  prefix = "mayur-unique-prefix"
+  project_id = "hc-53771aab9cd94f93ba281d03ab6"
+}
